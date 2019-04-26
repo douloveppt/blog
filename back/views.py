aceb08985b14bbb2ee2b1809b738442f4503705b
Wang Bibo <1551503184@qq.com>
2019-04-26 21:00:13 +0800

    新增人脸识别登录

--- back/views.py
+++ back/views.py
@@ -1,10 +1,14 @@
+import base64
 import datetime
 import math
+import os
 
 from flask import Blueprint, render_template, request, redirect, url_for, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from back.models import User, Article, Category, db
+from utils.faceid import register_face_user, match_face
 from utils.functions import is_login
+from utils.settings import IMAGE_DIR
 
 blue_back = Blueprint('back', __name__)
 
@@ -90,13 +94,22 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
         password2 = request.form.get('password2')
-        if username and password and password2:
+        face = request.form.get('face')
+        if username and password and password2 and face:
             user = User.query.filter(User.username == username).first()
             if user:
                 error = '该账号已被注册！'
                 return render_template('back/register.html', error=error)
             else:
+                img = face.split(',')[-1]
+                if not register_face_user(img, username):
+                    error = '注册失败'
+                    return render_template('back/register.html', error=error)
                 if password2 == password:
+                    file = base64.b64decode(img)
+                    img_dir = os.path.join(IMAGE_DIR, username + '.jpg')
+                    with open(img_dir, 'wb') as f:
+                        f.write(file)
                     user = User()
                     user.username = username
                     user.password = generate_password_hash(password)
@@ -106,7 +119,7 @@ def register():
                     error = '两次密码不一致'
                     return render_template('back/register.html', error=error)
         else:
-            error = '请填写完整信息！'
+            error = '信息不完整！'
             return render_template('back/register.html', error=error)
 
 
@@ -130,6 +143,30 @@ def login():
         else:
             error = '请填写完整的登录信息！'
             return render_template('back/login.html', error=error)
+
+
+@blue_back.route('/face_login/', methods=['GET', 'POST'])
+def face_login():
+    if request.method == 'GET':
+        return render_template('back/face_login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        face = request.form.get('face')
+        if username and face:
+            user = User.query.filter(User.username == username).first()
+            if not user:
+                error = '该账号不存在，请先注册！'
+                return render_template('back/login.html', error=error)
+            img = face.split(',')[-1]
+            file = os.path.join(IMAGE_DIR, username + '.jpg')
+            file_exists = os.path.exists(file)
+            if not file_exists:
+                return render_template('back/login.html', error='该账号没有保存的人脸信息')
+            score = match_face(img, file)
+            if score < 80:
+                return render_template('back/login.html', error='匹配不成功')
+            session['user_id'] = user.user_id
+            return redirect(url_for('back.index'))
 
 
 @blue_back.route('/logout/', methods=['GET'])
@@ -183,7 +220,8 @@ def modify(id):
     cate = art.category.cat_name
     if request.method == 'GET':
         cates = Category.query.all()
-        return render_template('back/mod_article.html', cates=cates, username=username, title=title, content=content, date=date, cate=cate)
+        return render_template('back/mod_article.html', cates=cates, username=username, title=title, content=content,
+                               date=date, cate=cate)
     if request.method == 'POST':
         title2 = request.form.get('title')
         content2 = request.form.get('content')
---
+++ utils/settings.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+IMAGE_DIR = BASE_DIR + '/static/back/images/user_faces'
